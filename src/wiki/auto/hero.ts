f36bd8318d184ffa8a91d04cd5119f04e369dbbc
Shiaupiau <stu43005@gmail.com>
2020-10-21 16:07:37 +0800

    feat(wiki-hero-bot): auto create all rank

--- src/wiki/auto/hero.ts
+++ src/wiki/auto/hero.ts
@@ -12,41 +12,39 @@ export async function wikiHeroBot(bot: MWBot) {
 			const hero = json[name];
 			if (!hero.enable) continue;
 
-			const originalText = await bot.readText(hero.name, true);
-			if (!originalText) {
-				// not exist
+			const heroExists = await bot.exists(hero.name);
+			if (!heroExists) {
 				await bot.create(hero.name, hero.hero);
 				logger.log(`Create: ${hero.name}`);
 
 				if (hero.name != hero.scName) {
-					await bot.create(hero.scName, `#重新導向 [[${hero.name}]]`, "簡中導向");
-					logger.log(`Redirect: ${hero.scName} -> ${hero.name}`);
-				}
-
-				for (const rankName in hero.ranks) {
-					if (hero.ranks.hasOwnProperty(rankName)) {
-						const rank = hero.ranks[rankName];
-						await bot.create(`模板:${rankName}`, rank);
-						logger.log(`Create: 模板:${rankName}`);
+					try {
+						await bot.create(hero.scName, `#重新導向 [[${hero.name}]]`, "簡中導向");
+						logger.log(`Redirect: ${hero.scName} -> ${hero.name}`);
+					} catch (error) {
 					}
 				}
 
 				if (hero.cv) {
 					const cvCat = `分類:聲優為 ${hero.cv} 的角色`;
-					const cvCatText = await bot.readText(cvCat, true);
-					if (!cvCatText) {
+					try {
 						await bot.create(cvCat, `{{角色聲優分類}}`);
+						logger.log(`Create: ${cvCat}`);
+					} catch (error) {
 					}
 				}
 			}
 
 			for (const rankName in hero.ranks) {
-				if (hero.ranks.hasOwnProperty(rankName) && rankName.includes('三階+')) {
-					try {
-						const rank = hero.ranks[rankName];
-						await bot.create(`模板:${rankName}`, rank);
-						logger.log(`Create: 模板:${rankName}`);
-					} catch (error) {
+				if (hero.ranks.hasOwnProperty(rankName)) {
+					const rankExists = await bot.exists(rankName);
+					if (!rankExists) {
+						try {
+							const rank = hero.ranks[rankName];
+							await bot.create(`模板:${rankName}`, rank);
+							logger.log(`Create: 模板:${rankName}`);
+						} catch (error) {
+						}
 					}
 				}
 			}
