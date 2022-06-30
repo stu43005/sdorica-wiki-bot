70d3795fff74f66eddfc930242f91ff86876f066
Shiaupiau <stu43005@gmail.com>
2022-07-01 00:37:17 +0800

    feat(wiki): 改進共鳴突破素材表

--- src/wiki/Resonance.ts
+++ src/wiki/Resonance.ts
@@ -13,10 +13,17 @@ export default function wikiResonance() {
 
 {| class="wikitable" style="text-align:center;"
 |-
-! 共鳴階級
-! style="background-color: #ffd700;" | [[庫倫]]
+! rowspan="2" | 共鳴階級
+! rowspan="2" style="background-color: #ffd700;" | [[庫倫]]
+! colspan="5" | 一般共鳴
+! colspan="5" | 替代共鳴
+|-
 ! 淚 !! 區域限定道具 !! 共鳴魂能 !! [[魂能結晶]] !! [[精煉魂能]]
-! style="background-color: #00ffff;" | [[起源魂石]]`;
+! style="background-color: #00ffff;" | [[起源魂石]]
+! style="background-color: #00ffff;" | [[精煉記憶石]]
+! style="background-color: #00ffff;" |
+! style="background-color: #00ffff;" |
+! style="background-color: #00ffff;" |`;
 	const ResonanceItems: Record<string, RowWrapper> = {};
 	for (let i = 0; i < RankUpItemRefsTable.length; i++) {
 		const row = RankUpItemRefsTable.get(i);
@@ -44,6 +51,10 @@ export default function wikiResonance() {
 		const item4 = ResonanceItems[row.get('item4Ref')];
 		const item5 = ResonanceItems[row.get('item5Ref')];
 		const ext1 = ResonanceItems[row.get('ext1Ref')];
+		const ext2 = ResonanceItems[row.get('ext2Ref')];
+		const ext3 = ResonanceItems[row.get('ext3Ref')];
+		const ext4 = ResonanceItems[row.get('ext4Ref')];
+		const ext5 = ResonanceItems[row.get('ext5Ref')];
 		if (category == 'Rank') {
 			const currRankName = rank()(rank1);
 			const privRankName = rank()(String(rank1 - 1));
@@ -60,17 +71,33 @@ export default function wikiResonance() {
 			if (item4) SubRankUpItemCount[item4.get('payLinkId')] = (SubRankUpItemCount[item4.get('payLinkId')] || 0) + row.get('item4Count');
 			if (item5) SubRankUpItemCount[item5.get('payLinkId')] = (SubRankUpItemCount[item5.get('payLinkId')] || 0) + row.get('item5Count');
 			if (ext1) SubRankUpItemCount[ext1.get('payLinkId')] = (SubRankUpItemCount[ext1.get('payLinkId')] || 0) + row.get('ext1Count');
+			if (ext2) SubRankUpItemCount[ext2.get('payLinkId')] = (SubRankUpItemCount[ext2.get('payLinkId')] || 0) + row.get('ext2Count');
+			if (ext3) SubRankUpItemCount[ext3.get('payLinkId')] = (SubRankUpItemCount[ext3.get('payLinkId')] || 0) + row.get('ext3Count');
+			if (ext4) SubRankUpItemCount[ext4.get('payLinkId')] = (SubRankUpItemCount[ext4.get('payLinkId')] || 0) + row.get('ext4Count');
+			if (ext5) SubRankUpItemCount[ext5.get('payLinkId')] = (SubRankUpItemCount[ext5.get('payLinkId')] || 0) + row.get('ext5Count');
+		}
+		const items: [RowWrapper, number][] = [
+			[item4, row.get('item4Count')],
+			[item2, row.get('item2Count')],
+			[item1, row.get('item1Count')],
+			[item3, row.get('item3Count')],
+			[item5, row.get('item5Count')],
+			[ext1, row.get('ext1Count')],
+			[ext2, row.get('ext2Count')],
+			[ext3, row.get('ext3Count')],
+			[ext4, row.get('ext4Count')],
+			[ext5, row.get('ext5Count')],
+		];
+		if (row.get('item4Ref') === 'CommonD') {
+			const temp = items[0];
+			items[0] = items[4];
+			items[4] = temp;
 		}
 		out += `
 | style="background-color: #ffd700;" | ${item2wiki('1002', row.get('coin'), false, ResonanceItemOptions)}
-| ${item4 && row.get('item4Ref') != 'CommonD' ? item2wikiWithType(item4.get('payType'), item4.get('payLinkId'), row.get('item4Count'), ResonanceItemOptions) : '-'}
-| ${item2 ? item2wikiWithType(item2.get('payType'), item2.get('payLinkId'), row.get('item2Count'), ResonanceItemOptions) : '-'}
-| ${item1 ? item2wikiWithType(item1.get('payType'), item1.get('payLinkId'), row.get('item1Count'), ResonanceItemOptions) : '-'}
-| ${item3 ? item2wikiWithType(item3.get('payType'), item3.get('payLinkId'), row.get('item3Count'), ResonanceItemOptions) : '-'}
-| ${item4 && row.get('item4Ref') == 'CommonD' ? item2wikiWithType(item4.get('payType'), item4.get('payLinkId'), row.get('item4Count'), ResonanceItemOptions) : item5 && row.get('item5Ref') == 'CommonD' ? item2wikiWithType(item5.get('payType'), item5.get('payLinkId'), row.get('item5Count'), ResonanceItemOptions) : '-'}
-| style="background-color: #00ffff;" | ${ext1 ? item2wikiWithType(ext1.get('payType'), ext1.get('payLinkId'), row.get('ext1Count'), ResonanceItemOptions) : '-'}`;
-	}
-	out += `
+${items.map((item, index) => `${index >= 5 ? `| style="background-color: #00ffff;" ` : ""}| ${item[0] ? item2wikiWithType(item[0].get('payType'), item[0].get('payLinkId'), item[1], ResonanceItemOptions) : '-'}`).join("\n")}`;
+		if (category == 'SubRank' && rank1 == 15) {
+			out += `
 |-
 ! +1 ~ +15<br/>合計
 ! style="background-color: #ffd700;" | ${item2wiki('1002', SubRankUpItemCount['1002'], false, ResonanceItemOptions)}
@@ -79,7 +106,13 @@ export default function wikiResonance() {
 ! ${item2wiki(ResonanceItems['CharItemF'].get('payLinkId'), SubRankUpItemCount[ResonanceItems['CharItemF'].get('payLinkId')], false, ResonanceItemOptions)}
 ! ${item2wiki(ResonanceItems['CharItemA'].get('payLinkId'), SubRankUpItemCount[ResonanceItems['CharItemA'].get('payLinkId')], false, ResonanceItemOptions)}
 ! ${item2wiki(ResonanceItems['CommonD'].get('payLinkId'), SubRankUpItemCount[ResonanceItems['CommonD'].get('payLinkId')], false, ResonanceItemOptions)}
-! style="background-color: #00ffff;" | ${item2wiki(ResonanceItems['CommonE'].get('payLinkId'), SubRankUpItemCount[ResonanceItems['CommonE'].get('payLinkId')], false, ResonanceItemOptions)}`;
+! style="background-color: #00ffff;" | ${item2wiki(ResonanceItems['CommonE'].get('payLinkId'), SubRankUpItemCount[ResonanceItems['CommonE'].get('payLinkId')], false, ResonanceItemOptions)}
+! style="background-color: #00ffff;" | -
+! style="background-color: #00ffff;" | -
+! style="background-color: #00ffff;" | -
+! style="background-color: #00ffff;" | -`;
+		}
+	}
 	const SublimationAngelia = SublimationTable.find(row => row.get('heroId') == '21'/* 安潔莉亞 */);
 	if (SublimationAngelia) {
 		out += `
@@ -91,7 +124,11 @@ export default function wikiResonance() {
 | -
 | ${item2wiki(SublimationAngelia.get('item2Id'), SublimationAngelia.get('item2Count'), false, ResonanceItemOptions)}
 | ➡️<br/>➡️<br/>➡️
-| style="background-color: #00ffff;" | ${item2wiki(SublimationAngelia.get('ItemID'), SublimationAngelia.get('ItemCount'), false, ResonanceItemOptions)}`;
+| style="background-color: #00ffff;" | ${item2wiki(SublimationAngelia.get('ItemID'), SublimationAngelia.get('ItemCount'), false, ResonanceItemOptions)}
+! style="background-color: #00ffff;" | -
+! style="background-color: #00ffff;" | -
+! style="background-color: #00ffff;" | -
+! style="background-color: #00ffff;" | -`;
 	}
 	out += `\n|}
 
