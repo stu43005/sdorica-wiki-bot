a97897a73687c885a669c7711a95a2a1319aff9c
Shiaupiau <stu43005@gmail.com>
2022-12-21 21:59:52 +0800

    feat(wiki): add Buffs

--- src/wiki.ts
+++ src/wiki.ts
@@ -18,6 +18,7 @@ import wikiAvatars from "./wiki/Avatars";
 import wikiBattlefieldDropItems from "./wiki/BattlefieldDropItems";
 import wikiBattlefieldRanks from "./wiki/BattlefieldRanks";
 import wikiBattlefields from "./wiki/Battlefields";
+import wikiBuffs from "./wiki/Buffs";
 import wikiChapter from "./wiki/Chapter";
 import wikiCharVoice from "./wiki/CharVoice";
 import wikiDiligents from "./wiki/Diligents";
@@ -44,17 +45,19 @@ import wikiTavernMissionDrop from "./wiki/TavernMissionDrop";
 import { wikiTips } from "./wiki/Tips";
 import wikiTreasureItems from "./wiki/TreasureItems";
 
-const logger = new Logger('wiki');
+const logger = new Logger("wiki");
 
 function wikiQuestsData() {
 	const questJson = getQuestJsonData();
-	const questsOut: string[] = Object.values(questJson).map((chapter) => {
-		const tempOut: string[] = [];
-		objectEach(chapter, (fullname, quest) => {
-			tempOut.push(`${fullname}\n\n${quest}`);
-		});
-		return tempOut;
-	}).reduce((prev: string[], curr: string[]) => prev.concat(curr), []);
+	const questsOut: string[] = Object.values(questJson)
+		.map((chapter) => {
+			const tempOut: string[] = [];
+			objectEach(chapter, (fullname, quest) => {
+				tempOut.push(`${fullname}\n\n${quest}`);
+			});
+			return tempOut;
+		})
+		.reduce((prev: string[], curr: string[]) => prev.concat(curr), []);
 	return questsOut.join("\n\n##############################\n\n");
 }
 
@@ -64,7 +67,9 @@ function wikiHeroesData() {
 	objectEach(heroJson, (name, hero) => {
 		let str = name;
 		str += `\n\n${hero.hero}\n\n${hero.base}`;
-		str += Object.values(hero.ranks).map((r) => `\n\n******************************\n\n${r}`).join("");
+		str += Object.values(hero.ranks)
+			.map((r) => `\n\n******************************\n\n${r}`)
+			.join("");
 		heroOut.push(str);
 	});
 	return heroOut.join("\n\n##############################\n\n");
@@ -72,7 +77,9 @@ function wikiHeroesData() {
 
 function wikiItemsData() {
 	const itemJson = getItemJsonData();
-	return Object.values(itemJson).map(v => Object.values(v).join("\n\n")).join("\n\n##############################\n\n");
+	return Object.values(itemJson)
+		.map((v) => Object.values(v).join("\n\n"))
+		.join("\n\n##############################\n\n");
 }
 
 function wrapHiddenDiv(content: string) {
@@ -80,21 +87,34 @@ function wrapHiddenDiv(content: string) {
 }
 
 async function applyHtmlTemplate(title: string, body: string) {
-	const template = await fs.readFile(path.join(__dirname, "./wiki/template.html"), { encoding: "utf8" });
+	const template = await fs.readFile(
+		path.join(__dirname, "./wiki/template.html"),
+		{ encoding: "utf8" }
+	);
 	return template.replace(/<%title%>/g, title).replace(/<%body%>/g, body);
 }
 
 async function outWiki(bot: MWBot | undefined, title: string, out: string) {
-	const ext = out.startsWith("#") ? "md" : out.startsWith("<") ? "html" : "txt";
+	const ext = out.startsWith("#")
+		? "md"
+		: out.startsWith("<")
+		? "html"
+		: "txt";
 	if (ext === "html") {
 		out = await applyHtmlTemplate(title, out);
 	}
-	await outText(path.join(WIKI_PATH, `${title.replace(/:/, '_')}.${ext}`), out);
+	await outText(
+		path.join(WIKI_PATH, `${title.replace(/:/, "_")}.${ext}`),
+		out
+	);
 	if (bot && ext === "txt") {
-		if (title.startsWith('模板:')) {
+		if (title.startsWith("模板:")) {
 			await bot.editOnDifference(title, out);
 		} else {
-			await bot.editOnDifference(`使用者:小飄飄/wiki/${title}`, wrapHiddenDiv(out));
+			await bot.editOnDifference(
+				`使用者:小飄飄/wiki/${title}`,
+				wrapHiddenDiv(out)
+			);
 		}
 	}
 }
@@ -102,13 +122,23 @@ async function outWiki(bot: MWBot | undefined, title: string, out: string) {
 async function outWikiJson(bot: MWBot | undefined, title: string, data: any) {
 	await outJson(path.join(WIKI_PATH, `${title}.json`), data);
 	if (bot) {
-		await bot.editOnDifference(`使用者:小飄飄/bot/${title}.json`, JSON.stringify(data, null, 4));
+		await bot.editOnDifference(
+			`使用者:小飄飄/bot/${title}.json`,
+			JSON.stringify(data, null, 4)
+		);
 	}
 }
 
-async function outWikiConstant(bot: MWBot | undefined, title: string, value: string) {
+async function outWikiConstant(
+	bot: MWBot | undefined,
+	title: string,
+	value: string
+) {
 	if (value && bot) {
-		await bot.editOnDifference(`模板:Constant/${title}`, `${value}<noinclude>{{Documentation}}</noinclude>`);
+		await bot.editOnDifference(
+			`模板:Constant/${title}`,
+			`${value}<noinclude>{{Documentation}}</noinclude>`
+		);
 	}
 }
 
@@ -122,52 +152,57 @@ export async function wikiMain(updateWiki?: boolean) {
 		logger.log(`[MWBOT] Login failed: ${error}`);
 	}
 
-	await outWiki(bot, 'AdvAchievements', wikiAdvAchievements());
-	await outWiki(bot, 'Adventure', wikiAdventure());
-	await outWiki(bot, 'AdventureRank', wikiAdventureRank());
-	await outWiki(bot, 'Avatars', wikiAvatars());
-	await outWiki(bot, 'Battlefields', wikiBattlefields());
-	await outWiki(bot, 'BattlefieldDropItems', wikiBattlefieldDropItems());
-	await outWiki(bot, 'BattlefieldRanks', wikiBattlefieldRanks());
-	await outWiki(bot, 'Chapter', wikiChapter());
-	await outWiki(bot, 'CharVoice', wikiCharVoice());
-	await outWiki(bot, 'Diligents', wikiDiligents());
-	await outWiki(bot, 'EvaluateAchievements', wikiEvaluateAchievements());
-	await outWiki(bot, 'Evaluates', wikiEvaluates());
-	await outWiki(bot, 'ExploreBuilding', wikiExploreBuilding());
-	await outWiki(bot, 'ExploreComposite', wikiExploreComposite());
-	await outWiki(bot, 'ExploreItems', wikiExploreItems());
-	await outWiki(bot, 'FreeHeroes', wikiFreeHeroes());
+	await outWiki(bot, "AdvAchievements", wikiAdvAchievements());
+	await outWiki(bot, "Adventure", wikiAdventure());
+	await outWiki(bot, "AdventureRank", wikiAdventureRank());
+	await outWiki(bot, "Avatars", wikiAvatars());
+	await outWiki(bot, "Battlefields", wikiBattlefields());
+	await outWiki(bot, "BattlefieldDropItems", wikiBattlefieldDropItems());
+	await outWiki(bot, "BattlefieldRanks", wikiBattlefieldRanks());
+	await outWiki(bot, "Buffs", wikiBuffs());
+	await outWiki(bot, "Chapter", wikiChapter());
+	await outWiki(bot, "CharVoice", wikiCharVoice());
+	await outWiki(bot, "Diligents", wikiDiligents());
+	await outWiki(bot, "EvaluateAchievements", wikiEvaluateAchievements());
+	await outWiki(bot, "Evaluates", wikiEvaluates());
+	await outWiki(bot, "ExploreBuilding", wikiExploreBuilding());
+	await outWiki(bot, "ExploreComposite", wikiExploreComposite());
+	await outWiki(bot, "ExploreItems", wikiExploreItems());
+	await outWiki(bot, "FreeHeroes", wikiFreeHeroes());
 	// await outWiki(bot, 'Gashapons', wikiGashapons());
 	// await outWikiJson(bot, 'Gashapons', wikiGashaponsJson());
-	await outWiki(bot, 'Heroes', wikiHeroes());
-	await outWikiJson(bot, 'Heroes', wikiHeroesJson());
-	await outWiki(bot, 'HomelandBuilding', wikiHomelandBuilding());
-	await outWiki(bot, 'HomelandMonster', wikiHomelandMonster());
-	await outWiki(bot, 'LevelUps', wikiLevelUps());
-	await outWiki(bot, 'Missions', wikiMissions());
-	await outWikiJson(bot, 'MonsterTrap', wikiMonsterTrapJson());
-	await outWiki(bot, 'QuestAchievements', wikiQuestAchievements());
-	await outWiki(bot, 'Resonance', wikiResonance());
-	await outWiki(bot, 'RewardGroups', wikiRewardGroups());
-	await outWiki(bot, 'SignInReward', wikiSignInReward());
+	await outWiki(bot, "Heroes", wikiHeroes());
+	await outWikiJson(bot, "Heroes", wikiHeroesJson());
+	await outWiki(bot, "HomelandBuilding", wikiHomelandBuilding());
+	await outWiki(bot, "HomelandMonster", wikiHomelandMonster());
+	await outWiki(bot, "LevelUps", wikiLevelUps());
+	await outWiki(bot, "Missions", wikiMissions());
+	await outWikiJson(bot, "MonsterTrap", wikiMonsterTrapJson());
+	await outWiki(bot, "QuestAchievements", wikiQuestAchievements());
+	await outWiki(bot, "Resonance", wikiResonance());
+	await outWiki(bot, "RewardGroups", wikiRewardGroups());
+	await outWiki(bot, "SignInReward", wikiSignInReward());
 	// await outWiki(bot, 'Store', wikiStore());
-	await outWiki(bot, 'TavernMission', wikiTavernMission());
-	await outWiki(bot, 'TavernMissionCompact', wikiTavernMissionCompact());
-	await outWiki(bot, 'TavernMissionDrop', wikiTavernMissionDrop());
-	await outWiki(bot, 'TreasureItems', wikiTreasureItems());
+	await outWiki(bot, "TavernMission", wikiTavernMission());
+	await outWiki(bot, "TavernMissionCompact", wikiTavernMissionCompact());
+	await outWiki(bot, "TavernMissionDrop", wikiTavernMissionDrop());
+	await outWiki(bot, "TreasureItems", wikiTreasureItems());
 
-	await outWiki(bot, 'Tips', wikiTips());
+	await outWiki(bot, "Tips", wikiTips());
 	// await outWiki(bot, '模板:Tips', wikiTipsTemplate());
 
-	await outWikiConstant(bot, 'MaxLevel', getConstants()('playerMaxLv'));
-	await outWikiConstant(bot, 'MaxResonanceLevel', getConstants()('subrankMax'));
+	await outWikiConstant(bot, "MaxLevel", getConstants()("playerMaxLv"));
+	await outWikiConstant(
+		bot,
+		"MaxResonanceLevel",
+		getConstants()("subrankMax")
+	);
 
 	await outText(path.join(WIKI_PATH, "raw/quests.txt"), wikiQuestsData());
 	await outText(path.join(WIKI_PATH, "raw/heroes.txt"), wikiHeroesData());
 	await outText(path.join(WIKI_PATH, "raw/items.txt"), wikiItemsData());
 
-	await outWiki(bot, 'index', await wikiIndex());
+	await outWiki(bot, "index", await wikiIndex());
 
 	if (bot) {
 		await wikiHeroBot(bot);
---
+++ src/wiki/Buffs.ts
@@ -0,0 +1,35 @@
+import { ImperiumData } from "../imperium-data";
+import { localizationString } from "../localization";
+import { wikiH1, wikiH3 } from "../templates/wikiheader";
+import { wikitable, WikiTableStruct } from "../templates/wikitable";
+
+const BuffInfoTable = ImperiumData.fromGamedata().getTable("BuffInfo");
+
+export default function wikiBuffs() {
+	let out = wikiH1("BUFF列表");
+
+	const table: WikiTableStruct = [["! id", "! 名稱", "! 說明", "! Icon"]];
+
+	for (const row of BuffInfoTable.rows.sort(
+		(a, b) => a.get("order") - b.get("order")
+	)) {
+		const id = row.get("id");
+		const name = localizationString("BaseBuff")(
+			row.get("localizationNameKey")
+		);
+		const info = localizationString("BaseBuff")(
+			row.get("localizationInfoKey")
+		);
+		const iconKey = row.get("iconKey");
+		table.push({
+			attributes: row.get("viewable")
+				? ``
+				: `style="background-color: #ccc; color: #1e1e1e;" title="不可見"`,
+			ceils: [id, wikiH3(name, id, true), info, iconKey],
+		});
+	}
+
+	out += `\n\n${wikitable(table)}`;
+
+	return out;
+}
