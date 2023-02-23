c048d3e1c9f1d22350e98a18d59059ecf9ee3a5a
Shiaupiau <stu43005@gmail.com>
2023-02-24 00:23:40 +0800

    feat(wiki): Heroes使用角色系列進行群組化

--- src/wiki/Heroes.ts
+++ src/wiki/Heroes.ts
@@ -2,11 +2,14 @@ import _ from "lodash";
 import { Hero } from "../model/hero";
 import { heroSlotIconTemplate } from "../templates/hero-slot-icon";
 import { wikiH1, wikiH2 } from "../templates/wikiheader";
+import { wikiPageLink } from "../templates/wikilink";
 import { wikitable, WikiTableStruct } from "../templates/wikitable";
 import { sortByCharacterModelNo } from "../utils";
 
 function getSortedHeroes() {
 	return Hero.getAll().sort((a, b) => {
+		if (a.enable && !b.enable) return -1;
+		if (!a.enable && b.enable) return 1;
 		if (a.empty && !b.empty) return 1;
 		if (!a.empty && b.empty) return -1;
 		return sortByCharacterModelNo(a.model, b.model);
@@ -14,12 +17,11 @@ function getSortedHeroes() {
 }
 
 export default function wikiHeroes() {
-	let out = wikiH1('英雄角色');
+	let out = wikiH1("英雄角色");
 
 	const sortedHeroes = getSortedHeroes();
-	const grouppedHeroes = _.groupBy(
-		sortedHeroes,
-		hero => hero.enable ? "一般" : hero.empty ? "環境" : "停用",
+	const grouppedHeroes = _.groupBy(sortedHeroes, (hero) =>
+		hero.enable ? hero.series : hero.empty ? "環境" : "停用"
 	);
 
 	for (const [groupId, group] of Object.entries(grouppedHeroes)) {
@@ -41,21 +43,36 @@ export default function wikiHeroes() {
 			],
 		};
 		for (const hero of group) {
-			const skillbooks = hero.books.map(book => book.bookItem?.toWiki({ text: "" }) ?? '');
+			const skillbooks = hero.books.map(
+				(book) => book.bookItem?.toWiki({ text: "" }) ?? ""
+			);
 
 			table.rows.push({
-				attributes: hero.enable ? "" : hero.empty ? `style="background-color: #90ee90; color: #1e1e1e;" title="環境"` : `style="background-color: #ccc; color: #1e1e1e;" title="停用"`,
+				attributes: hero.enable
+					? ""
+					: hero.empty
+					? `style="background-color: #90ee90; color: #1e1e1e;" title="環境"`
+					: `style="background-color: #ccc; color: #1e1e1e;" title="停用"`,
 				ceils: [
 					hero.id,
 					hero.model,
 					hero.empty ? "環境" : heroSlotIconTemplate(hero.slot),
 					hero.toWiki(),
-					skillbooks.join(''),
+					skillbooks.join(""),
 					hero.atk,
 					hero.hp,
-					hero.revive || '',
-					hero.storyChapter?.title ? `[[${hero.firstname}《${hero.storyChapter.title}》|${hero.storyChapter.title}]]` : "",
-					hero.resonanceItem?.toWiki({ text: "", count: undefined }) ?? "",
+					hero.revive || "",
+					hero.storyChapter?.title
+						? wikiPageLink(
+								"Chapter",
+								hero.storyChapter.title,
+								hero.storyChapter.id
+						  )
+						: "",
+					hero.resonanceItem?.toWiki({
+						text: "",
+						count: undefined,
+					}) ?? "",
 				],
 			});
 		}
@@ -68,5 +85,7 @@ export default function wikiHeroes() {
 
 export function wikiHeroesJson() {
 	const sortedHeroes = getSortedHeroes();
-	return sortedHeroes.filter(hero => hero.enable).map(hero => hero.toJSON());
+	return sortedHeroes
+		.filter((hero) => hero.enable)
+		.map((hero) => hero.toJSON());
 }
